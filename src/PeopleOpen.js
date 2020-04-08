e0aa9290290e93dcecfa550f2e89fb222fec4456
GGGODLIN <qwe70301@gmail.com>
2020-04-08 18:55:21 +0800

    0408

--- src/PeopleOpen.js
+++ src/PeopleOpen.js
@@ -28,7 +28,7 @@ import {
 
 import DeviceInfo from 'react-native-device-info';
 
-import {Button} from 'react-native-elements';
+import {Button, CheckBox} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import md5 from 'md5';
 import moment from 'moment';
@@ -39,6 +39,10 @@ const PeopleOpen = props => {
   const [count3, setCount3] = useState(0);
   const [count4, setCount4] = useState(0);
   const [count5, setCount5] = useState(0);
+  const [IsDinner, setIsDinner] = useState(false);
+  const [IsBathe, setIsBathe] = useState(false);
+  const [IsAccommodation, setIsAccommodation] = useState(false);
+  const [todo, settodo] = useState(0);
 
   const testApi = async () => {
     let url = `http://daycare.southeastasia.cloudapp.azure.com:9800/sihu/api/CaseReportApi/RequestService`;
@@ -67,6 +71,57 @@ const PeopleOpen = props => {
       .then(response => response.json())
       .then(res => {
         console.log('TEST AJAX', res);
+        Alert.alert('成功', res.Message, [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]);
+      })
+      .catch(err =>
+        Alert.alert('網路異常，請稍後再試...', ' ', [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]),
+      );
+  };
+
+  const testApi2 = async () => {
+    let url = `http://daycare.southeastasia.cloudapp.azure.com:9800/sihu/api/CaseReportApi/RequestService`;
+
+    console.log(`Making submit request to: ${url}`);
+    let md5People = md5(props.people.CaseIdentity);
+    let now = new Date();
+    let formatNow = moment(now).format('YYYY-MM-DDTHH:mm:ss');
+    console.log('before hash', props.people.CaseIdentity);
+    console.log('md5 Hash is', md5People);
+    console.log('NOW', now, typeof now);
+    console.log('FORMAT', formatNow, typeof formatNow);
+
+    const data = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        IdentityId: md5People,
+        ServiceDate: formatNow,
+        ServiceCode: 'S02',
+        SignOut: true,
+        AllDay: true,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log('TEST2 AJAX', res);
+        Alert.alert('成功', res.Message, [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]);
       })
       .catch(err =>
         Alert.alert('網路異常，請稍後再試...', ' ', [
@@ -85,13 +140,144 @@ const PeopleOpen = props => {
     let md5People = md5(props.people.CaseIdentity);
     let now = new Date();
     let formatNow = moment(now).format('YYYY-MM-DDTHH:mm:ss');
-    let IsHeartbeat = count5===0?false:true;
-    let IsBloodPressure = (count4===0||count3===0)?false:true;
-    let IsTemperature = count===0?false:true;
+    let IsHeartbeat = count5 === 0 ? false : true;
+    let IsBloodPressure = count4 === 0 || count3 === 0 ? false : true;
+    let IsTemperature = count === 0 ? false : true;
     console.log('before hash', props.people.CaseIdentity);
     console.log('md5 Hash is', md5People);
-    console.log('NOW', now, typeof now);
     console.log('FORMAT', formatNow, typeof formatNow);
+
+    if(todo===1){
+      const dataIn = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        IdentityId: md5People,
+        ServiceDate: formatNow,
+        ServiceCode: 'S01',
+        Reporting: true,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log('In AJAX', res);
+        
+      })
+      .catch(err =>
+        Alert.alert('網路異常，請稍後再試...', ' ', [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]),
+      );
+    }
+
+    if(todo===3){
+      const dataOut = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        IdentityId: md5People,
+        ServiceDate: formatNow,
+        ServiceCode: 'S02',
+        SignOut: true,
+        AllDay: true,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log('Out AJAX', res);
+        
+      })
+      .catch(err =>
+        Alert.alert('網路異常，請稍後再試...', ' ', [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]),
+      );
+    }
+
+    const data2 = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        IdentityId: md5People,
+        ServiceDate: formatNow,
+        ServiceCode: 'S03',
+        IsDinner: IsDinner,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log('POST DATA2 AJAX', res);
+      })
+      .catch(err =>
+        Alert.alert('網路異常，請稍後再試...', ' ', [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]),
+      );
+
+    const data3 = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        IdentityId: md5People,
+        ServiceDate: formatNow,
+        ServiceCode: 'S04',
+        IsBathe: IsBathe,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log('POST DATA3 AJAX', res);
+      })
+      .catch(err =>
+        Alert.alert('網路異常，請稍後再試...', ' ', [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]),
+      );
+
+    const data4 = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        IdentityId: md5People,
+        ServiceDate: formatNow,
+        ServiceCode: 'S05',
+        IsAccommodation: IsAccommodation,
+      }),
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log('POST DATA4 AJAX', res);
+      })
+      .catch(err =>
+        Alert.alert('網路異常，請稍後再試...', ' ', [
+          {
+            text: '確定',
+            onPress: () => {},
+          },
+        ]),
+      );
 
     const data = await fetch(url, {
       method: 'POST',
@@ -114,6 +300,14 @@ const PeopleOpen = props => {
       .then(response => response.json())
       .then(res => {
         console.log('POST DATA AJAX', res);
+        Alert.alert('成功', res.Message, [
+          {
+            text: '確定',
+            onPress: () => {
+              goBackToPeople();
+            },
+          },
+        ]);
       })
       .catch(err =>
         Alert.alert('網路異常，請稍後再試...', ' ', [
@@ -162,8 +356,30 @@ const PeopleOpen = props => {
         <View style={styles.body}>
           <View style={styles.sectionContainer}>
             <Button title={props.people.CaseName} titleStyle={{fontSize: 25}} />
+            <CheckBox
+              title="報到(量體溫)"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={todo === 1}
+              onPress={() => settodo(1)}
+            />
+            <CheckBox
+              title="僅量體溫"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={todo === 2}
+              onPress={() => settodo(2)}
+            />
+            <CheckBox
+              title="簽退(量體溫)"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={todo === 3}
+              onPress={() => settodo(3)}
+            />
           </View>
-          <View style={styles.sectionContainer}>
+          <View
+            style={todo === 0 ? {display: 'none'} : styles.sectionContainer}>
             <Button title={`額溫:    ${count}`} titleStyle={{fontSize: 25}} />
             {/*<Button
               title={`環境溫度:    ${count2}`}
@@ -181,25 +397,46 @@ const PeopleOpen = props => {
               title={`心率值:    ${count5}`}
               titleStyle={{fontSize: 25}}
             />
-          </View>
-        </View>
-        
-        <View style={styles.body}>
-          <View style={styles.sectionContainer}>
-            <Button
-              title={'報到'}
-              titleStyle={{fontSize: 25}}
-              buttonStyle={{backgroundColor:'orange'}}
-              onPress={() => {
-                testApi();
-              }}
+
+            <CheckBox
+              center
+              title="晚餐"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={IsDinner}
+              onPress={() => setIsDinner(!IsDinner)}
             />
+            <CheckBox
+              center
+              title="沐浴"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={IsBathe}
+              onPress={() => setIsBathe(!IsBathe)}
+            />
+            <CheckBox
+              center
+              title="住宿"
+              checkedIcon="dot-circle-o"
+              uncheckedIcon="circle-o"
+              checked={IsAccommodation}
+              onPress={() => setIsAccommodation(!IsAccommodation)}
+            />
+
             <Button
               title={'送出結果'}
               titleStyle={{fontSize: 25}}
-              buttonStyle={{backgroundColor:'orange'}}
+              buttonStyle={{backgroundColor: 'orange'}}
               onPress={() => {
-                postDataApi();
+                Alert.alert('確定送出?', '', [
+          {
+            text: '確定',
+            onPress: () => {
+              postDataApi();
+            },
+          },
+        ]);
+                
               }}
             />
           </View>
@@ -207,7 +444,7 @@ const PeopleOpen = props => {
             <Button
               title={'返回清單'}
               titleStyle={{fontSize: 25}}
-              buttonStyle={{backgroundColor:'orange'}}
+              buttonStyle={{backgroundColor: 'orange'}}
               onPress={() => {
                 goBackToPeople();
               }}
